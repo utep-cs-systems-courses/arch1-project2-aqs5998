18eecbfcb1dfda97c0546445af90acb4c17e7a77
aqs5998 <aqs5998@gmail.com>
2020-03-07 21:50:23 -0700

    song works

---
+++ demos/buzzer_demo/stateMachines.c
@@ -0,0 +1,81 @@
+#include <msp430.h>
+#include "stateMachines.h"
+#include <stddef.h>
+#include "buzzer.h"
+#include "libTimer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define a 440
+char state = 0;
+
+void playSong() {
+  //buzzer_set_period(1000);
+  //int notes[] = {1000, 0, 810, 740, 0, 810, 830, 845, 840, 845, 0, 0, 0, 790, 770, 0, 2000};
+  // for(int i = 0; notes[i]!=790;i++){
+  // buzzer_set_period(notes[i]);
+  // }
+
+  static char state = 0;
+  switch(state){
+  case 0:
+    state = 1;
+    buzzer_set_period(523);//C
+    break;
+  case 1:
+    state = 2;
+    buzzer_set_period(523);//C
+    break;
+  case 2:
+    state = 3;
+    buzzer_set_period(500);//D
+    break;
+  case 3:
+    state = 4;
+    buzzer_set_period(450);//E
+    break;
+  case 4:
+    state = 5;
+    buzzer_set_period(450);//E
+    break;
+  case 5:
+    state = 6;
+    buzzer_set_period(500);//D
+    break;
+  case 6:
+    state = 7;
+    buzzer_set_period(523);//C
+    break;
+  case 7:
+    state = 8;
+    buzzer_set_period(600);//B
+    break;
+  case 8:
+    state = 9;
+    buzzer_set_period(650);//A
+    break;
+  case 9:
+    state = 10;
+    buzzer_set_period(650);//A
+    break;
+  case 10:
+    state = 11;
+    buzzer_set_period(600);//B
+    break;
+  case 11:
+    state = 12;
+    buzzer_set_period(523);//c
+    break;
+  case 12:
+    state = 13;
+    buzzer_set_period(600);//B
+    break;
+  case 13:
+    state = 14;
+    buzzer_set_period(600);
+    break;
+  case 14:
+    state = 15;
+    buzzer_set_period(0);
+    break;
+  }
+}
