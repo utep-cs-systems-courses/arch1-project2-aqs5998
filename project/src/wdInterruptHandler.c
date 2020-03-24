681070f1343b14dff38b0ca51ed21bafc1543328
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-03-24 13:12:49 -0600

    slowed down song

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -7,7 +7,7 @@ void
 __interrupt_vec(WDT_VECTOR) WDT(){
   static char blink_count = 0;
   playSong();
-  if (++blink_count == 2) {
+  if (++blink_count == 3) {
     // state_advance();
     // buzzer_set_period(1000);
     if(button2_state_down == 1){//Play music and blink with first button.
