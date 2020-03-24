2e2ce34ad1d5ea791400222c124d9a672a4fdf7e
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-03-24 13:17:44 -0600

    Updated tempo again

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -6,7 +6,7 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){
   static char blink_count = 0;
-  if (++blink_count == 5) {
+  if (++blink_count == 3) {
     // state_advance();
     // buzzer_set_period(1000);
     playSong();
