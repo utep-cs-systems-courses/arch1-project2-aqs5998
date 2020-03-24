5953dc0a3f827327a898a17182ec6ee7239a4d1a
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-03-24 12:52:34 -0600

    More updates

--- project/src/buzzerMain.c
+++ project/src/buzzerMain.c
@@ -1,13 +1,14 @@
 #include <msp430.h>
+#include <msp430g2553.h>
 #include "libTimer.h"
-#include "buzzer.h"
-#include "stateMachines.h"
+#include "led.h"
+#include "switches.h"
 
 int main() {
     configureClocks();
- 
     buzzer_init();
-    playSong();
+    switch_init();
+    //playSong();
     // buzzer_set_period(2000);
     enableWDTInterrupts();
     or_sr(0x18);          // CPU off, GIE on
--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -8,7 +8,9 @@ __interrupt_vec(WDT_VECTOR) WDT(){
   if (++blink_count == 2) {
     // state_advance();
     // buzzer_set_period(1000);
-    playSong();
+    if(button2_state_down == 1){//Play music and blink with first button.
+      playSong();
+    }
     blink_count = 0;
   }
 }
