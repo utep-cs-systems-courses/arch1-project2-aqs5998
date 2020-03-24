28b5c458a7df242c741f5431e68d32eceaf3b6ce
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-03-24 13:17:53 -0600

    Removed playSong in main

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -9,7 +9,7 @@ __interrupt_vec(WDT_VECTOR) WDT(){
   if (++blink_count == 3) {
     // state_advance();
     // buzzer_set_period(1000);
-    playSong();
+    //playSong();
     if(button2_state_down == 1){//Play music and blink with first button.
       playSong();
     }
