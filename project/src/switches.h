d2836dc8810fb886464543c67fdfd6e496ac54ca
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-03-24 13:10:15 -0600

    updated switches.h

--- project/src/switches.h
+++ project/src/switches.h
@@ -2,9 +2,9 @@
 #define switches_included
 
 #define SW1 BIT0                /* switch1 is p1.3 */
-#define SW2 BIT2
-//#define SW3 BIT2
-//#define SW4 BIT3
+#define SW2 BIT1
+#define SW3 BIT2
+#define SW4 BIT3
 
 #define SWITCHES (SW1|SW2|SW3|SW3)
 //#define SWITCHES SW2/* only 3 switchs on this board */
