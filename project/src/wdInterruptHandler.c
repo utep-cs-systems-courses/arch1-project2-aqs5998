e7aea0265351f71a057b35c9664a1889ed1cf9dd
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-03-24 13:29:28 -0600

    Updated wd

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -6,14 +6,14 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){
   static char blink_count = 0;
-  if (++blink_count == 3) {
+  //if (++blink_count == 3) {
     // state_advance();
     // buzzer_set_period(1000);
     //playSong();
-    if(songStatus == 1){//Play music and blink with first button.
+    if(songStatus == 1 && ++blink_count ==3){//Play music and blink with first button.
       playSong();
       blink_count=0;
     }
     blink_count = 0;
-  }
+  //}
 }
