0a6172ec122001b377dbfe05ab5e352a2b171f82
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-03-24 14:48:43 -0600

    again

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -12,7 +12,7 @@ __interrupt_vec(WDT_VECTOR) WDT(){
       blink_count = 0;
     }
   }
-  if(songState == 4){
+  if(button4_state_down == 4){
     buzzer_set_period(10);
     blink_count = 0;
     songState = 0;
