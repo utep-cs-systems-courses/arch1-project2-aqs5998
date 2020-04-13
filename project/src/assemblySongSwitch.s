a190aab9400e4413e28ebed9690926c79e225cf4
Alex Sanchez <alex@Alexs-Air.elp.rr.com>
2020-04-13 16:20:57 -0600

    test again

--- project/src/assemblySongSwitch.s
+++ project/src/assemblySongSwitch.s
@@ -20,9 +20,80 @@ case01:
         mov #523, r12
         call #buzzer_set_period
         jmp end;
-default:
+case02:
+        mov #2, &state;
         mov #523, r12
         call #buzzer_set_period
+        jmp end
+case03:
+        mov #3, &state;
+        mov #500, r12
+        call #buzzer_set_period ; //D
+        jmp end
+case04:
+        mov #4, &state;
+        mov #450, r12
+        call #buzzer_set_period; //E
+        jmp end
+case05:
+        mov #5, &state;
+        mov #450, r12
+        call #buzzer_set_period ; //E
+        jmp end;
+case06:
+        mov #6, &state;
+        mov #500, r12
+        call #buzzer_set_period; //D
+        jmp end;
+case07:
+        mov #7, &state;
+        mov #523, r12
+        call #buzzer_set_period; //C
+        jmp end;
+case08:
+        mov #8, &state;
+        mov #600, r12
+        call #buzzer_set_period ; //B
+        jmp end;
+case09:
+        mov #9, &state;
+        mov #650, r12
+        call #buzzer_set_period ; //A
+        jmp end;
+case10:
+        mov #10, &state;
+        mov #650, r12
+        call #buzzer_set_period; //A
+        jmp end
+case11:
+        mov #11, &state;
+        mov #600, r12
+        call #buzzer_set_period ; //B
+        jmp end;
+case12:
+        mov #12, &state;
+        mov #523, r12
+        call #buzzer_set_period; //c
+        jmp end;
+case13:
+        mov #13, &state;
+        mov #600, r12
+        call #buzzer_set_period; //B
+        jmp end;
+case14:
+        mov #14, &state;
+        mov #600, r12
+        call #buzzer_set_period;
+        jmp end;
+case15:
+        mov #15, &state;
+        mov #0, r12
+        call #buzzer_set_period;
+        call #buzzer_init;
+        jmp end;
+default:
+        mov #600, r12
+        call #buzzer_set_period;
 
 end: 
         ret
