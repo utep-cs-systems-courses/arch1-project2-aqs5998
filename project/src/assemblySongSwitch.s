9a66472718faf5db5771a59210ef49f966582c4d
Alex Sanchez <alex@Alexs-Air.elp.rr.com>
2020-04-13 16:22:49 -0600

    another test

--- project/src/assemblySongSwitch.s
+++ project/src/assemblySongSwitch.s
@@ -14,12 +14,13 @@ state_adv:
 
         mov &state, r12
         add r12, r12 ; we are doing this to double the amount of "cells"
-        mov jt(r12), r0 ;moving the data to the first position ro
+        mov jt(r12), r0 ;moving the data to the first position 
+
 case01:
         mov #1, &state
         mov #523, r12
         call #buzzer_set_period
-        jmp end;
+        ;jmp end;
 case02:
         mov #2, &state;
         mov #523, r12
