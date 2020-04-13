d5e28bd0edb262c360c7bfccc0144661ccd4ea9f
Alex Sanchez <alex@Alexs-Air.elp.rr.com>
2020-04-13 16:01:41 -0600

    More changes

--- project/src/assemblySongSwitch.s
+++ project/src/assemblySongSwitch.s
@@ -36,60 +36,74 @@ case01:
         jmp end;
 case02:
         mov #2, &state;
-        call #buzzer_set_period(523)
+        mov #523, r12
+        call #buzzer_set_period
         jmp end
 case03:
         mov #3, &state;
-        call #buzzer_set_period(500); //D
+        mov #500, r12
+        call #buzzer_set_period ; //D
         jmp end
 case04:
         mov #4, &state;
+        mov #450, r12
         call #buzzer_set_period(450); //E
         jmp end
 case05:
         mov #5, &state;
-        call #buzzer_set_period(450); //E
+        mov #450, r12
+        call #buzzer_set_period ; //E
         jmp end;
 case06:
         mov #6, &state;
+        mov #500, r12
         call #buzzer_set_period(500); //D
         jmp end;
 case07:
         mov #7, &state;
+        mov #523, r12
         call #buzzer_set_period(523); //C
         jmp end;
 case08:
         mov #8, &state;
-        call #buzzer_set_period(600); //B
+        mov #600, r12
+        call #buzzer_set_period ; //B
         jmp end;
 case09:
         mov #9, &state;
-        call #buzzer_set_period(650); //A
+        mov #650, r12
+        call #buzzer_set_period ; //A
         jmp end;
 case10:
         mov #10, &state;
-        jmp end;
-        call #buzzer_set_period(650); //A
+        mov #650, r12
+        call #buzzer_set_period; //A
+        jmp end
 case11:
         mov #11, &state;
-        call #buzzer_set_period(600); //B
+        mov #600, r12
+        call #buzzer_set_period ; //B
         jmp end;
 case12:
         mov #12, &state;
+        mov #523, r12
         call #buzzer_set_period(523); //c
         jmp end;
 case13:
         mov #13, &state;
+        mov #600, r12
         call #buzzer_set_period(600); //B
         jmp end;
 case14:
         mov #14, &state;
+        mov #600, r12
         call #buzzer_set_period(600);
         jmp end;
 case15:
         mov #15, &state;
-        call #buzzer_set_period(0);
-        buzzer_init();
+        mov #0, r12
+        call #buzzer_set_period;
+        call #buzzer_init;
         jmp end;
 default:
         call #buzzer_init();
